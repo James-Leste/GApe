6841b112ea29ed7a6455e10d6644e50bbc4e8ee0
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-11-25 22:00:27 +0200

    rebase changes from Ali

--- app/userdata/page.tsx
+++ app/userdata/page.tsx
@@ -5,7 +5,27 @@ import React, { useEffect, useState } from 'react'
 import { createClient } from '@/utils/supabase/client'
 import { Button } from '@/components/ui/button'
 import { User } from '@supabase/supabase-js'
-import { randomUUID } from 'crypto'
+import InfoBlock from '@/components/blocks/info'
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from '@/components/ui/sheet'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+    )
+}
 
 const supabase = createClient()
 
@@ -60,6 +80,45 @@ const UserDataPage: React.FC = () => {
                 <Button>hello</Button>
                 <Button onClick={insert}>create new canvas</Button>
                 <Button onClick={showdata}>add new item</Button>
+            </div>
+            <div>
+                <div className='flex flex-row gap-5'>
+                    <InfoBlock></InfoBlock>
+                    <InfoBlock></InfoBlock>
+                    <InfoBlock></InfoBlock>
+                    <InfoBlock></InfoBlock>
+                </div>
+            </div>
+            <div>
+                <Counter></Counter>
+                <Counter></Counter>
+            </div>
+            <div>
+                <div className='grid grid-cols-2 gap-2'>
+                    <Sheet key='bottom' modal={false}>
+                        <SheetTrigger asChild>
+                            <Button variant='outline'>bottom</Button>
+                        </SheetTrigger>
+                        <SheetContent side='bottom' className='bg-white'>
+                            {/* <SheetHeader>
+                                <SheetTitle>Edit profile</SheetTitle>
+                                <SheetDescription>
+                                    Make changes to your profile here. Click
+                                    save when you're done.
+                                </SheetDescription>
+                            </SheetHeader>
+                            <div className='grid gap-4 py-4'></div> */}
+                            <div className='flex flex-row gap-5'>
+                                <InfoBlock key={1}></InfoBlock>
+                                <InfoBlock key={2}></InfoBlock>
+                                <InfoBlock key={3}></InfoBlock>
+                                <InfoBlock key={4}></InfoBlock>
+                            </div>
+
+                            <SheetFooter></SheetFooter>
+                        </SheetContent>
+                    </Sheet>
+                </div>
             </div>
         </div>
     )
