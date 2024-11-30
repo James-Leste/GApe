33a4f22e5e0d125357a1a655857298d11d47f484
Alimjan <alimujiang1414@qq.com>
2024-11-30 16:57:33 +0200

    style: new size for Skills block

--- components/blocks/v2/profile/SkillsCard.tsx
+++ components/blocks/v2/profile/SkillsCard.tsx
@@ -3,18 +3,18 @@
 import * as React from 'react'
 import { SkillsCardProps } from './types'
 
-export const SkillsCard: React.FC<SkillsCardProps> = ({ categories }) => {
+export const SkillsBlock_L: React.FC<SkillsCardProps> = ({ categories }) => {
     return (
-        <div className='flex flex-col grow px-2 py-2.5 w-full text-xs font-medium leading-loose bg-white rounded-lg border border-solid border-slate-400 min-h-[236px]'>
+        <div className='flex flex-col grow gap-2 px-2 py-2.5 text-xs  leading-loose bg-white rounded-lg border border-solid border-slate-400 w-96 h-[236px]'>
             {categories.map((category, index) => (
                 <div
                     key={index}
-                    className='flex flex-col pb-2 w-full border-b border-gray-300'
+                    className='flex flex-col w-full border-b border-gray-300'
                 >
                     <div className='flex flex-col w-full whitespace-nowrap'>
                         <div className='flex items-start w-full text-neutral-800'>
-                            <div className='flex gap-2 items-center w-[198px]'>
-                                <div className='gap-2.5 self-stretch px-1 my-auto rounded-lg border border-solid border-neutral-300'>
+                            <div className='flex  items-center'>
+                                <div className=' self-stretch leading-4 px-1  rounded-lg font-medium border border-solid border-neutral-300'>
                                     {category.name}
                                 </div>
                             </div>
@@ -22,10 +22,45 @@ export const SkillsCard: React.FC<SkillsCardProps> = ({ categories }) => {
                         <div className='flex flex-wrap gap-0.5 items-start self-start mt-1 min-h-[20px] text-neutral-500'>
                             {category.skills.map((skill, skillIndex) => (
                                 <React.Fragment key={skillIndex}>
-                                    <div>{skill}</div>
+                                    <div className='leading-4'>{skill}</div>
                                     {skillIndex <
                                         category.skills.length - 1 && (
-                                        <div>|</div>
+                                        <div className='leading-4'>|</div>
+                                    )}
+                                </React.Fragment>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export const SkillsBlock_M: React.FC<SkillsCardProps> = ({ categories }) => {
+      const categoriesForMedium = [categories[0], categories[categories.length - 1]]
+    return (
+        <div className='flex flex-col grow gap-2 justify-center px-2 py-2.5  text-xs font-medium leading-loose bg-white rounded-lg border border-solid border-slate-400  w-96 h-[140px]'>
+            {categoriesForMedium.map((category, index) => (
+                <div
+                    key={index}
+                    className='flex flex-col w-full border-b border-gray-300'
+                >
+                    <div className='flex flex-col w-full whitespace-nowrap'>
+                        <div className='flex items-start w-full text-neutral-800'>
+                            <div className='flex  items-center'>
+                                <div className=' self-stretch leading-4 px-1  rounded-lg font-medium border border-solid border-neutral-300'>
+                                    {category.name}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex flex-wrap gap-0.5 items-start self-start mt-1 min-h-[20px] text-neutral-500'>
+                            {category.skills.map((skill, skillIndex) => (
+                                <React.Fragment key={skillIndex}>
+                                    <div className='leading-4'>{skill}</div>
+                                    {skillIndex <
+                                        category.skills.length - 1 && (
+                                        <div className='leading-4'>|</div>
                                     )}
                                 </React.Fragment>
                             ))}
