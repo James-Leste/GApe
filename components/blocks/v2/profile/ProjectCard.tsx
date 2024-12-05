e723b9ff8cea5c12c357a0e455c4ef1bf2ef2454
Alimjan <alimujiang1414@qq.com>
2024-12-05 10:59:58 +0200

    fix: use static svg for icons

--- components/blocks/v2/profile/ProjectCard.tsx
+++ components/blocks/v2/profile/ProjectCard.tsx
@@ -64,12 +64,36 @@ export const ProjectBlock_L: React.FC<ProjectCardProps> = ({
             </div>
             <div className='flex gap-10 justify-between items-center mt-1.5 w-full text-sm font-medium leading-none'>
                 <div className='flex gap-1 items-center self-stretch my-auto text-black'>
-                    <img
-                        loading='lazy'
-                        src='https://cdn.builder.io/api/v1/image/assets/TEMP/dc71ab0f216929cfa8c383cb604ee2b1b39e6ca864c17ed382074e93a5876767?placeholderIfAbsent=true&apiKey=cf0d30cc7fd245a4886607b2fdd26b2b'
+                <svg
+                        width='16'
+                        height='16'
+                        viewBox='0 0 48 48'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
                         className='object-contain shrink-0 self-stretch my-auto w-4 aspect-square'
-                        alt=''
-                    />
+                    >
+                        <path
+                            d='M9.85786 32.7574C6.23858 33.8432 4 35.3432 4 37C4 40.3137 12.9543 43 24 43C35.0457 43 44 40.3137 44 37C44 35.3432 41.7614 33.8432 38.1421 32.7574'
+                            stroke='#254d3e'
+                            stroke-width='4'
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                        />
+                        <path
+                            d='M24 35C24 35 37 26.504 37 16.6818C37 9.67784 31.1797 4 24 4C16.8203 4 11 9.67784 11 16.6818C11 26.504 24 35 24 35Z'
+                            fill='none'
+                            stroke='#254d3e'
+                            stroke-width='4'
+                            stroke-linejoin='round'
+                        />
+                        <path
+                            d='M24 22C26.7614 22 29 19.7614 29 17C29 14.2386 26.7614 12 24 12C21.2386 12 19 14.2386 19 17C19 19.7614 21.2386 22 24 22Z'
+                            fill='none'
+                            stroke='#254d3e'
+                            stroke-width='4'
+                            stroke-linejoin='round'
+                        />
+                    </svg>
                     <div className='self-stretch my-auto'>{location}</div>
                 </div>
                 <div className='self-stretch my-auto text-gray-700'>
@@ -137,12 +161,36 @@ export const ProjectBlock_M: React.FC<ProjectCardProps> = ({
             </div>
             <div className='flex  justify-between items-center  w-full text-sm font-medium leading-none'>
                 <div className='flex gap-1 items-center self-stretch my-auto text-black'>
-                    <img
-                        loading='lazy'
-                        src='https://cdn.builder.io/api/v1/image/assets/TEMP/dc71ab0f216929cfa8c383cb604ee2b1b39e6ca864c17ed382074e93a5876767?placeholderIfAbsent=true&apiKey=cf0d30cc7fd245a4886607b2fdd26b2b'
+                <svg
+                        width='16'
+                        height='16'
+                        viewBox='0 0 48 48'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
                         className='object-contain shrink-0 self-stretch my-auto w-4 aspect-square'
-                        alt=''
-                    />
+                    >
+                        <path
+                            d='M9.85786 32.7574C6.23858 33.8432 4 35.3432 4 37C4 40.3137 12.9543 43 24 43C35.0457 43 44 40.3137 44 37C44 35.3432 41.7614 33.8432 38.1421 32.7574'
+                            stroke='#254d3e'
+                            stroke-width='4'
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                        />
+                        <path
+                            d='M24 35C24 35 37 26.504 37 16.6818C37 9.67784 31.1797 4 24 4C16.8203 4 11 9.67784 11 16.6818C11 26.504 24 35 24 35Z'
+                            fill='none'
+                            stroke='#254d3e'
+                            stroke-width='4'
+                            stroke-linejoin='round'
+                        />
+                        <path
+                            d='M24 22C26.7614 22 29 19.7614 29 17C29 14.2386 26.7614 12 24 12C21.2386 12 19 14.2386 19 17C19 19.7614 21.2386 22 24 22Z'
+                            fill='none'
+                            stroke='#254d3e'
+                            stroke-width='4'
+                            stroke-linejoin='round'
+                        />
+                    </svg>
                     <div className='self-stretch my-auto'>{location}</div>
                 </div>
                 <div className='self-stretch my-auto text-gray-700'>
