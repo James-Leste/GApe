556051173b2c07d457c80c1dbc23be9b2b4ab020
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-12-06 19:37:03 +0200

    feat: finalize the creating and deleting blocks from the canvas

--- types/dbtypes.ts
+++ types/dbtypes.ts
@@ -52,36 +52,6 @@ export interface Block {
     isBig: boolean
 }
 
-// export interface InfoBlockData {
-//     type: string
-//     name: string
-//     description: string
-//     image: string
-//     url: string
-//     phone: string
-//     email: string
-//     github: string
-//     linkedin: string
-//     x: string
-//     tags: string[]
-// }
-
-// export interface EduBlockData {
-//     type: string
-//     institutionName: string
-//     location: string
-//     degree: string
-//     startDate: string
-//     endDate: string
-//     major: string
-//     minor?: string
-//     gpa?: string
-//     description: string
-//     tags: string[]
-//     image: string
-//     url: string
-// }
-
 export interface ProfileCardProps {
     name: string
     title: string
