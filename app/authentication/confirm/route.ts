0b58a68de608962414670ebcfbcbf114aaa8530a
Ziqi Wang <wangziqi2385823041@outlook.com>
2024-12-13 16:05:30 +0200

    feat: recover the email authentication feature

--- app/authentication/confirm/route.ts
+++ app/authentication/confirm/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: NextRequest) {
         })
         if (!error) {
             // redirect user to specified redirect URL or root of app
-            redirect(next)
+            redirect(`${next}?success=true`)
         }
     }
 
